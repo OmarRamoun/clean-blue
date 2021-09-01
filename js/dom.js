1e4b1e24943c7b3a388d25716493b0d34b693645
MrRamoun <omartarekramoun@gmail.com>
2021-09-01 15:17:14 +0200

    Add close button layout, style & functionality

--- js/dom.js
+++ js/dom.js
@@ -3,10 +3,9 @@ const button = document.querySelector('#menu-icon');
 function displayMenu() {
   const nav = document.querySelector('nav');
   nav.classList.toggle('sm-view');
-  console.log("click works");
-  const image = document.querySelector('svg');
-  // if (image.getAttribute(src) === "")
+  const menuIcon = document.querySelector('#menu-icon');
+  menuIcon.childNodes[0].classList.toggle('none');
+  menuIcon.childNodes[1].classList.toggle('none');
 }
 
-console.log("js works");
 button.addEventListener('click', displayMenu);
