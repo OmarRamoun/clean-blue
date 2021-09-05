a4d6697cbd89099ec6f12c49c495cde391f38057
MrRamoun <omartarekramoun@gmail.com>
2021-09-05 02:03:52 +0200

    MOdify event listener of the submit event

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 // Gets a reference to the form element, assuming
 // it contains the ID attribute "contact-form".
 const form = document.getElementById('contact-form');
+const email = form.querySelector('input[type="email"]');
 
 // Adds a listener for the "submit" event.
 form.addEventListener('submit', (event) => {
   // Prevents the browser from submitting the form
   // and thus unloading the current page.
-  event.preventDefault();
+  console.log('event listener');
+  if (!checkLower(email.value)) {
+    event.preventDefault();
+  } else {
+    // Creates a timeout to call submitForm after one second.
+    // eslint-disable-next-line no-use-before-define
+    setTimeout(submitForm, 1000);
 
-  // Creates a timeout to call submitForm after one second.
-  // eslint-disable-next-line no-use-before-define
-  setTimeout(submitForm, 1000);
+    // Monitors whether or not the form has been submitted.
+    // This prevents the form from being submitted twice in cases
+    // where the event callback function fires normally.
+    let formSubmitted = false;
 
-  // Monitors whether or not the form has been submitted.
-  // This prevents the form from being submitted twice in cases
-  // where the event callback function fires normally.
-  let formSubmitted = false;
-
-  function submitForm() {
-    if (!formSubmitted) {
-      formSubmitted = true;
-      form.submit();
+    function submitForm() {
+      if (!formSubmitted) {
+        formSubmitted = true;
+        form.submit();
+      }
     }
-  }
 
-  // Sends the event to Google Analytics and
-  // resubmits the form once the hit is done.
-  gtag('event', 'signup_form_complete', {
-    event_callback: submitForm,
-  });
+    // Sends the event to Google Analytics and
+    // resubmits the form once the hit is done.
+    gtag('event', 'signup_form_complete', {
+      event_callback: submitForm,
+    });
+  }
 });
 
 // eslint-disable-next-line camelcase
