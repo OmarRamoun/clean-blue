1fff86b8f1fcf721d50983e620c8a488cd12161b
MrRamoun <omartarekramoun@gmail.com>
2021-09-05 03:47:30 +0200

    Use ES6 arrow functions instead fo ES5 function declaration

--- js/load.js
+++ js/load.js
@@ -198,12 +198,12 @@ for (let i = 1; i <= 4; i += 1) {
   // works.push(myWork);
 }
 
-function closeModal() {
+const closeModal = () => {
   const modal = document.querySelector('.modal-container');
   modal.remove();
-}
+};
 
-function displayPopup(work) {
+const displayPopup = (work) => {
   const modal = document.createElement('div');
   modal.classList.add('modal-container', 'flex', 'cross-center', 'main-center');
 
@@ -316,7 +316,7 @@ function displayPopup(work) {
   }
 
   document.body.appendChild(modal);
-}
+};
 
 const seeButtons = document.querySelectorAll('.see');
 
