94f0808a2d9c9b33241fbac876eb3c90a465f562
MrRamoun <omartarekramoun@gmail.com>
2021-11-18 01:06:50 +0200

    Make the displayMenu function only work on small screens

--- js/dom.js
+++ js/dom.js
@@ -1,11 +1,13 @@
 const button = document.querySelector('#menu-icon');
 
 function displayMenu() {
-  const nav = document.querySelector('nav');
-  nav.classList.toggle('sm-view');
-  const menuIcon = document.querySelector('#menu-icon');
-  menuIcon.childNodes[0].classList.toggle('none');
-  menuIcon.childNodes[1].classList.toggle('none');
+  if (window.innerWidth < 768) {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('sm-view');
+    const menuIcon = document.querySelector('#menu-icon');
+    menuIcon.childNodes[0].classList.toggle('none');
+    menuIcon.childNodes[1].classList.toggle('none');
+  }
 }
 
 button.addEventListener('click', displayMenu);
